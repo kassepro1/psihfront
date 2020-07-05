813d665b9314a24e1a4602fc5dce4ddfdcc36a1e
kassepro <momartallakasse@gmail.com>
2020-07-05 16:23:43 +0100

    test add user with protractor

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
 
 ## Development server
+Run `npm run e2e` for testing add user dialog .
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import {browser, By, logging, protractor} from 'protractor';
 
-describe('workspace-project App', () => {
+/*describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -20,4 +20,79 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry));
   });
+});*/
+
+
+// tslint:disable-next-line:only-arrow-functions
+describe('add AppUser', function() {
+
+  browser.get('http://localhost:4200/users');
+
+  // tslint:disable-next-line:only-arrow-functions
+  it('should render addorupdateuser page', function() {
+
+// Checking the current url
+
+    // tslint:disable-next-line:prefer-const
+    // tslint:disable-next-line:prefer-const
+    let currentUrl = browser.driver.getCurrentUrl();
+
+    expect(currentUrl).toMatch('/users');
+    const addUser = browser.driver.findElement(By.name('addUser'));
+
+    addUser.click();
+  });
+
+  // tslint:disable-next-line:only-arrow-functions
+  it('should add User', function() {
+
+// Find page elements
+
+    const firstnameField = browser.driver.findElement(By.name('firstname'));
+    const lastnameField = browser.driver.findElement(By.name('lastname'));
+    const usernameField = browser.driver.findElement(By.name('username'));
+    const emailField = browser.driver.findElement(By.name('email'));
+
+// finding button to add user
+
+    const addUserButton = browser.driver.findElement(By.name('addorupordateUser'));
+
+// Fill input fields
+
+    console.log('entering firstname');
+
+    firstnameField.sendKeys('Momar Talla');
+
+    console.log('entering lastname');
+
+    lastnameField.sendKeys('KASSE');
+
+    console.log('entering username');
+
+    usernameField.sendKeys('kassepro');
+
+    console.log('entering email');
+
+    emailField.sendKeys('momartallakasse@gmail.com');
+
+// Ensure fields contains what we've entered
+
+    expect(firstnameField.getAttribute('value')).toEqual('Momar Talla');
+
+    expect(lastnameField.getAttribute('value')).toEqual('KASSE');
+
+    expect(usernameField.getAttribute('value')).toEqual('kassepro');
+
+    expect(emailField.getAttribute('value')).toEqual('momartallakasse@gmail.com');
+
+    console.log('clicking addUser button');
+
+    addUserButton.submit();
+
+    console.log('user added');
+
+    browser.waitForAngular();
+
+  });
+
 });
